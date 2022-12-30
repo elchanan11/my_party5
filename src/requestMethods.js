b8b3c87db43ae29e108058698b3fdcd4a1d23abc
elchanan11 <elchanan.elsner@gmail.com>
2022-12-30 12:14:57 +0200

    added api link

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL ="http://localhost:5000/api/"
+const BASE_URL ="https://myparty55.herokuapp.com/api/"
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTlkMzFjNTI2MGI0M2EzNWZkYTA4ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3MjE0ODY2MSwiZXhwIjoxNjcyNDA3ODYxfQ.lzqUJmw-fMFKuePvbofghtlFzFaluPRdKV32FH-Gx1I"
 
 export const publicRequest = axios.create({
