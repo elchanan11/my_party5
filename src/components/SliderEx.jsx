3f31c9d70959796a2f1aa77cdbcd9cb6b9cdacd4
elchanan11 <elchanan.elsner@gmail.com>
2023-02-15 17:52:02 +0200

    adding funal categories

--- src/components/SliderEx.jsx
+++ src/components/SliderEx.jsx
@@ -8,19 +8,6 @@ import {slideImages} from "../data";
 
 SwiperCore.use([EffectCoverflow, Autoplay, Pagination]);
 
-// if you want to use array
-const slide_img = [
-    "https://swiperjs.com/demos/images/nature-1.jpg",
-    "https://swiperjs.com/demos/images/nature-2.jpg",
-    "https://swiperjs.com/demos/images/nature-3.jpg",
-    "https://swiperjs.com/demos/images/nature-4.jpg",
-    "https://swiperjs.com/demos/images/nature-5.jpg",
-    "https://swiperjs.com/demos/images/nature-6.jpg",
-    "https://swiperjs.com/demos/images/nature-7.jpg",
-    "https://swiperjs.com/demos/images/nature-8.jpg",
-    "https://swiperjs.com/demos/images/nature-9.jpg",
-];
-
 const SliderEx = () => {
     return (
         <div className="main-swiper">
@@ -52,35 +39,6 @@ const SliderEx = () => {
                     );
                 })}
 
-                {/* or use normally  */}
-
-                {/* <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" alt="" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" alt="" />
-        </SwiperSlide> */}
             </Swiper>
         </div>
     );
--- src/data.js
+++ src/data.js
@@ -101,7 +101,7 @@ export const mainCategories = [
     {
         id:5,
         img:choklateIcon,
-        title:"פרלינים שוקולד",
+        title:"שוקולדים",
         cat: "MainChokolates"
     },
     {
@@ -119,8 +119,8 @@ export const mainCategories = [
     {
         id:8,
         img:extraIcon,
-        title:"שונות",
-        cat: "MainExtra"
+        title:"הדפסה אישית",
+        cat: "personalPrinting"
     },
     {
         id:9,
@@ -134,15 +134,15 @@ export const mainCategories = [
 export const subCategiries = [
     {
         id:1,
-        title:"זרי בלוני הליום",
+        title:"זריי בלוני הליום",
         cat: "HeliumBalloonGarlands",
         subCat: "MainBalloonsEvents"
     },
     {
         id:2,
         img:CategoryItem,
-        title:"בלוני בועה עם כיתוב",
-        cat: "BubbleBalloonsInscription",
+        title:"בלונים בהדפסה אישית",
+        cat: "personalPrintingBaloons",
         subCat: "MainBalloonsEvents"
     },
     {
@@ -162,34 +162,34 @@ export const subCategiries = [
     {
         id:5,
         img:CategoryItem,
-        title:"מארז בלון פורח",
-        cat: "HotAirBalloonCase",
+        title:"אביזרים לבלונים",
+        cat: "equitmentForBloons",
         subCat: "MainBalloonsEvents"
     },
     {
         id:6,
         img:CategoryItem,
-        title:"ציוד לבלונים",
-        cat: "EquipmentForBalloons",
+        title:"מוצרים נלווים לבלונים",
+        cat: "sideProductsForBalloons",
         subCat: "MainBalloonsEvents"
     },
     {
         id:7,
         img:CategoryItem,
-        title:"מתנות קטנות",
-        cat: "SmallGifts",
+        title:"מתנות",
+        cat: "gifts",
         subCat: "MainGifts"
     },
     {
         id:8,
         img:CategoryItem,
-        title:"מתנות בהדפסה אישית",
-        cat: "PersonalizedPrintedGifts",
+        title:"מנורות",
+        cat: "lightGifts",
         subCat: "MainGifts"
     },
     {
         id:9,
-        title:"מתנות לנערות",
+        title:"מתנות לילדות ונערות",
         cat: "GiftsForGirls",
         subCat: "MainGifts"
     },
@@ -203,28 +203,21 @@ export const subCategiries = [
     {
         id:11,
         img:CategoryItem,
-        title:"מתנות לגבר",
+        title:"מתנות לבעל",
         cat: "GiftsForMen",
         subCat: "MainGifts"
     },
     {
         id:12,
         img:CategoryItem,
-        title:"מתנות למשפחה",
-        cat: "giftsForTheFamily",
-        subCat: "MainGifts"
-    },
-    {
-        id:13,
-        img:CategoryItem,
-        title:"מתנות ליומולדת",
-        cat: "birthdayPresents",
+        title:"מתנות החלמה מהירה",
+        cat: "giftsForFastRecovery",
         subCat: "MainGifts"
     },
     {
         id:14,
         img:CategoryItem,
-        title:"מתנות ללידה",
+        title:"מתנות לידה",
         cat: "giftsForBirth",
         subCat: "MainGifts"
     },
@@ -236,101 +229,115 @@ export const subCategiries = [
         subCat: "MainGifts"
     },
     {
-        id:16,
-        img:CategoryItem,
-        title:"מתנות אהבה",
-        cat: "giftsLove",
-        subCat: "MainGifts"
-    },
-    {
         id:17,
         img:CategoryItem,
-        title:"מתנות לאירוסין",
+        title:"מתנות מאורסים",
         cat: "EngagementGifts",
         subCat: "MainGifts"
     },
     {
         id:18,
         img:CategoryItem,
-        title:"דף סוכר לעוגה",
-        cat: "SugarSheetForCake",
+        title:"דף אכיל לעוגה",
+        cat: "etableSheetForCake",
         subCat: "MainForCake"
     },
     {
         id:19,
         img:CategoryItem,
-        title:"דף סוכר לעוגה חלאקה",
-        cat: "SugarSheetForCakeHalaka",
+        title:"דף טרנספר לעוגה",
+        cat: "TransferPage",
         subCat: "MainForCake"
     },
     {
         id:20,
         img:CategoryItem,
-        title:"דף טרנספר",
-        cat: "TransferPage",
+        title:"נרות מספר",
+        cat: "candelsCake",
         subCat: "MainForCake"
     },
     {
         id:21,
-        img:CategoryItem,
-        title:"קישוטים לעוגה",
-        cat: "CakeDecorations",
-        subCat: "MainForCake"
-    },
-    {
-        id:22,
         img:CategoryItem,
         title:"נרות לעוגה",
         cat: "CakeCandles",
         subCat: "MainForCake"
     },
     {
+        id:22,
+        img:CategoryItem,
+        title:"זיקוקי מספר",
+        cat: "fireWorksNumberCake",
+        subCat: "MainForCake"
+    },
+    {
+        id:80,
+        img:CategoryItem,
+        title:"טופרים לעוגה",
+        cat: "cForCake",
+        subCat: "MainForCake"
+    },
+    {
         id:23,
         img:CategoryItem,
-        title:"חד פעמי לפי צבעים",
-        cat: "ForkByColors",
+        title:"זוהרים למסיבות",
+        cat: "lightsParty",
         subCat: "MainForParty"
     },
     {
         id:24,
         img:CategoryItem,
-        title:"חד פעמי מותגים",
-        cat: "specialFork",
+        title:"תותחי קונפטי",
+        cat: "confetyParty",
         subCat: "MainForParty"
     },
     {
         id:25,
         img:CategoryItem,
-        title:"וילונות פרנזים",
-        cat: "FringeCurtains",
+        title:"זיקוקים",
+        cat: "fireworksParty",
         subCat: "MainForParty"
     },
     {
         id:26,
         img:CategoryItem,
-        title:"דגלונים",
-        cat: "pennants",
+        title:"כלים חד פעמיים",
+        cat: "forkAndKnifeForParty",
         subCat: "MainForParty"
     },
     {
         id:27,
         img:CategoryItem,
-        title:"קונפטי",
-        cat: "Confetti",
+        title:"כלי הגשה",
+        cat: "ServingDishesParty",
         subCat: "MainForParty"
     },
     {
         id:28,
         img:CategoryItem,
-        title:"זוהרים למסיבה",
-        cat: "GlamorousParty",
+        title:"קישוטים למסיבה",
+        cat: "decorationsParty",
         subCat: "MainForParty"
     },
     {
         id:29,
         img:CategoryItem,
-        title:"זיקוקים",
-        cat: "fireworks",
+        title:"קישוטים ליומולדת",
+        cat: "decorationsBirthDay",
+        subCat: "MainForParty"
+    },
+    {
+        id:98,
+        img:CategoryItem,
+        title:"סרטי סאטן",
+        cat: "SatinibbonsParty",
+        subCat: "MainForParty"
+    },
+    {
+        id:99,
+        img:CategoryItem,
+        title:"מפות חד פעמיות",
+        cat: "disposableMapsParty",
         subCat: "MainForParty"
     },
     {
@@ -342,26 +349,199 @@ export const subCategiries = [
     {
         id:31,
         img:CategoryItem,
-        title:`פרלינים בלגיים "houser" `,
+        title:`פרלינים "hauser" `,
         cat: "BelgianPralines",
-        subCat: "MainChokolates"
-    },
-    {
-        id:32,
-        img:CategoryItem,
-        title:"מארזי שוקולד",
-        cat: "ChocolateBoxes",
         subCat: "MainChokolates"
     },
     {
         id:33,
         img:CategoryItem,
-        title:"שקיות שוקולד",
+        title:"שקולדים בצלופן",
         cat: "ChocolateBags",
         subCat: "MainChokolates"
     },
     {
-        id:34,
+        id:35,
+
+        title:"בקבוקים",
+        cat: "bottles",
+        subCat: "personalPrinting"
+    },
+    {
+        id:36,
+
+        title:"כוסות",
+        cat: "glsses",
+        subCat: "personalPrinting"
+    },
+    {
+        id:37,
+
+        title:"מברשות שיער",
+        cat: "hair",
+        subCat: "personalPrinting"
+    },
+    {
+        id:38,
+
+        title:"קופסאות",
+        cat: "boxes",
+        subCat: "personalPrinting"
+    },
+    {
+        id:39,
+
+        title:"מוצרים עם כיתוב מדבקה",
+        cat: "productsWithStickerText",
+        subCat: "personalPrinting"
+    },
+    {
+        id:40,
+        title:"דף סוכר לעוגה",
+        cat: "sugerPageForCake",
+        subCat: "personalPrinting"
+    },
+    {
+        id:41,
+        title:"שמפניה + כוסות",
+        cat: "shampeinAndGlasses",
+        subCat: "MBoxes"
+    },
+    {
+        id:42,
+        title:"כוסות עם כיתוב",
+        cat: "glssesWithTextt",
+        subCat: "MBoxes"
+    },
+    {
+        id:43,
+
+        title:"דובים עם שוקולד",
+        cat: "bears",
+        subCat: "MBoxes"
+    },
+    {
+        id:44,
+
+        title:"מארז בלון פורח",
+        cat: "flyingBloonBox",
+        subCat: "MBoxes"
+    },
+    {
+        id:45,
+
+        title:"מארזי לידה",
+        cat: "birthBox",
+        subCat: "MBoxes"
+    },
+    {
+        id:46,
+
+        title:"קופסאות למארזים",
+        cat: "boxesForPackeges",
+        subCat: "MBoxes"
+    },
+    {
+        id:47,
+        title:"מבצעים",
+        cat: "shampeinAndGlasses",
+        subCat: "MainSales"
+    },
+    {
+        id:48,
+        title:"דובים במבצע",
+        cat: "glssesWithTextt",
+        subCat: "MainSales"
+    },
+    {
+        id:49,
+
+        title:"מארזים במבצע",
+        cat: "bears",
+        subCat: "MainSales"
+    },
+    {
+        id:50,
+
+        title:"בלונים במבצע",
+        cat: "flyingBloonBox",
+        subCat: "MainSales"
+    },
+    {
+        id:51,
+
+        title:"מוצרים במבצע",
+        cat: "birthBox",
+        subCat: "MainSales"
+    },
+    {
+        id:52,
+        title:"מתנות במבצע",
+        cat: "boxesForPackeges",
+        subCat: "MainSales"
+    },
+    {
+        id:53,
+
+        title:"שנה טובה",
+        cat: "goodYear",
+        subCat: "MainHolydays"
+    },
+    {
+        id:54,
+
+        title:"סוכות",
+        cat: "holiday",
+        subCat: "MainHolydays"
+    },
+    {
+        id:55,
+
+        title:"חנוכה",
+        cat: "holiday2",
+        subCat: "MainHolydays"
+    },
+    {
+        id:56,
+        title:"פורים",
+        cat: "hokiday3",
+        subCat: "MainHolydays"
+    },
+    {
+        id:57,
+        title:"פסח",
+        cat: "hokiday4",
+        subCat: "MainHolydays"
+    },
+    {
+        id:58,
+
+        title:"שבועות",
+        cat: "hokiday5",
+        subCat: "MainHolydays"
+    },
+    {
+        id:59,
+
+        title:"קיץ",
+        cat: "summer",
+        subCat: "MainHolydays"
+    },
+    {
+        id:60,
+
+        title:"חורף",
+        cat: "winter",
+        subCat: "MainHolydays"
+    },
+    {
+        id:61,
+        title:"שונות",
+        cat: "different",
+        subCat: "MainHolydays"
+    },
+    {
+        id:62,
 
         title:"מומלצים",
         cat: "Reccomanded",
